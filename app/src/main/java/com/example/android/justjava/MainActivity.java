03af5a193e8f5197a41a9d5872e024186b1b6f52
Albert Bustamante <albertbustamante@hotmail.com>
2018-01-05 09:50:11 -0600

    Updated MainActivity.java and activity_main.xml

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findVigit ewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
